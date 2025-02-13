87c131d3106fcb12c29de4ab92c3fdca50ced5bd
Saidur289 <saidur.riaz1@gmailcomm>
2025-02-14 03:20:02 +0600

    basic about multiple reducers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {createStore} = require('redux')
+const {createStore, combineReducers} = require('redux')
 // defineing variable 
 // const INCREMENT = 'INCREMENT'
 // const DECREMENT = 'DECREMENT'
@@ -126,33 +126,116 @@ const {createStore} = require('redux')
 // store.dispatch(incrementCounterByValue(5))
 // store.dispatch(incrementCounterByValue(10))
 // example - 3
-const ADD_USER = "ADD_USER"
-const initialUserState = {
-    users: ['saidur'],
-    count: 1
+// const ADD_USER = "ADD_USER"
+// const initialUserState = {
+//     users: ['saidur'],
+//     count: 1
+// }
+// const addUser = (user) => {
+//     return {
+//         type: ADD_USER,
+//         payload: user
+//     }
+// }
+// const usersReducers = (state = initialUserState, action) => {
+//     switch (action.type) {
+//         case ADD_USER:
+            
+//             return {
+//                 users: [...state.users, action.payload],
+//                 count: state.count + 1
+//             }
+    
+//         default:
+//             state;
+//     }
+// }
+// const store = createStore(usersReducers)
+// store.subscribe(() => {
+//     console.log(store.getState())
+// })
+// store.dispatch(addUser('Rahman'))
+// store.dispatch(addUser('sahed'))
+// example - 4 
+// define constant 
+const ADD_PRODUCT = 'ADD_PRODUCT'
+const GET_PRODUCT = 'GET_PRODUCT'
+const GET_CART = 'GET_CART'
+const ADD_CART = 'ADD_CART'
+// DECLARE STATE 
+const initialProductState = {
+    product: ['sugar', 'gram'],
+    count: 2
 }
-const addUser = (user) => {
-    return {
-        type: ADD_USER,
-        payload: user
+const initialCartsState = {
+    carts: ['pen', 'pencil'],
+    count: 2
+}
+// CREATE ACTION 
+const getProduct = () => {
+    return{
+        type: GET_PRODUCT
     }
 }
-const usersReducers = (state = initialUserState, action) => {
+const AddProduct = (product) => {
+    return{
+        type: ADD_PRODUCT,
+        payload: product
+    }
+}
+const getCarts = () => {
+    return{
+        type: GET_CART
+    }
+}
+const AddCarts = (cart) => {
+    return{
+        type: ADD_CART,
+        payload: cart
+    }
+}
+// CREATE REDUCERS 
+const productReducers = (state = initialProductState, action) => {
     switch (action.type) {
-        case ADD_USER:
+        case GET_PRODUCT:
             
-            return {
-                users: [...state.users, action.payload],
-                count: state.count + 1
+            return{
+                ...state
             }
+            case ADD_PRODUCT:
+                return{
+                    products: [...state.product, action.payload],
+                    count: state.count + 1
+                }
     
         default:
-            state;
+            return state
     }
 }
-const store = createStore(usersReducers)
-store.subscribe(() => {
-    console.log(store.getState())
+const cartsReducers = (state = initialCartsState, action) => {
+    switch (action.type) {
+        case GET_CART:
+            
+            return{
+                ...state
+            }
+            case ADD_CART:
+                return{
+                    products: [...state.carts, action.payload],
+                    count: state.count + 1
+                }
+    
+        default:
+            return state
+    }
+}
+const rootReducer = combineReducers({
+    productR: productReducers,
+    cartsR: cartsReducers
 })
-store.dispatch(addUser('Rahman'))
-store.dispatch(addUser('sahed'))
+const store = createStore(rootReducer)
+store.subscribe(() => {
+    console.log(store.getState());
+})
+store.dispatch(getProduct())
+store.dispatch(AddCarts('kire jogon'))
