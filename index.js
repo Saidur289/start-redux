865d61e9839bab9176bbc9e708672b8bdecd3957
Saidur289 <saidur.riaz1@gmailcomm>
2025-02-14 02:10:38 +0600

    create mini counter function

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
+const {createStore} = require('redux')
 // defineing variable 
+// const INCREMENT = 'INCREMENT'
+// const DECREMENT = 'DECREMENT'
+// const ADD_USER = 'ADD_USER'
+// // state 
+// const initialCounterState = {
+//     count: 0,
+// }
+// const initialUserState = {
+//     users: [{name: 'saidur rahman'}]
+// }
+
+
+// // action  have two state 
+// // action have two things object and payload
+// const incrementCounter = () => {
+//     return {
+//         type: INCREMENT
+//     }
+// }
+// const decrementCounter = () => {
+//     return {
+//         type: DECREMENT
+//     }
+// }
+// const addUser = () => {
+//     return {
+//         type: ADD_USER,
+//         payload: {name: 'saidur'}
+//     }
+// }
+// // 1. state
+// // 2.dispatch action 
+// // 3. reducer
+// const counterReducer = (state = initialCounterState, action) => {
+//     switch(action.type){
+//         case INCREMENT :
+//         return {
+//             ...state,
+//             count: state.count + 1
+//         }
+//         case DECREMENT :
+//             return {
+//                 ...state,
+//                 count: state.count - 1
+//             }
+//             default:
+//                 state;
+//     }
+// }
+// // create store 
+// const store = createStore(counterReducer)
+// store.subscribe(() => {
+//     console.log(store.getState());
+// })
+// // dispatch 
+// store.dispatch(incrementCounter())
+// store.dispatch(incrementCounter())
+// store.dispatch(incrementCounter())
+// store.dispatch(decrementCounter())
+// example - 2 recap 
 const INCREMENT = 'INCREMENT'
 const DECREMENT = 'DECREMENT'
-const ADD_USER = 'ADD_USER'
-// state 
+const RESET = "RESET"
+// CREATE STATE 
 const initialCounterState = {
     count: 0,
 }
-const initialUserState = {
-    users: [{name: 'saidur rahman'}]
-}
-
-
-// action  have two state 
-// action have two things object and payload
+// ACTION START HERE  
 const incrementCounter = () => {
     return {
         type: INCREMENT
@@ -23,28 +78,37 @@ const decrementCounter = () => {
         type: DECREMENT
     }
 }
-const addUser = () => {
+const resetCounter = () => {
     return {
-        type: ADD_USER,
-        payload: {name: 'saidur'}
+        type: RESET
     }
 }
-// 1. state
-// 2.dispatch action 
-// 3. reducer
+// CREATE REDUCERS 
 const counterReducer = (state = initialCounterState, action) => {
-    switch(action.type){
-        case INCREMENT :
-        return {
-            ...state,
-            count: state.count + 1
-        }
-        case DECREMENT :
+    switch (action.type) {
+        case INCREMENT:
             return {
-                ...state,
-                count: state.count - 1
+                ...state, count: state.count + 1
             }
-            default:
-                state;
+            
+           case DECREMENT:
+            return {
+                ...state, count : state.count - 1
+            }
+            case RESET:
+                return{
+                    ...state, count: 0
+                }
+    
+        default:
+            state;
     }
 }
+// CREATE STORE 
+const store = createStore(counterReducer)
+store.subscribe(() => {
+    console.log(store.getState());
+})
+store.dispatch(incrementCounter())
+store.dispatch(decrementCounter())
+store.dispatch(resetCounter())
